b645eb564d1267c98e6f0e6ffcafbb1246b53e6e
Richard Peterson <webbuyrich@gmail.com>
2016-01-17 19:47:56 -0600

    placed parsed JSON in employees variable. console log employees with typeof

--- js/widget.js
+++ js/widget.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function(){
 	if(xhr.readyState === 4){		
-			console.log(xhr.responseText);		
+			var employees = JSON.parse(xhr.responseText);	
+			console.log(typeof employees);
 	}
 };
 xhr.open('GET', 'data/employees.json');
