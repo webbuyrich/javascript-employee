7738c51072992cd1661fee69246182ea46a2192b
Richard Peterson <webbuyrich@gmail.com>
2016-01-17 20:52:16 -0600

    added loop for employees. output list to index.html. app complete!

--- js/widget.js
+++ js/widget.js
@@ -5,8 +5,20 @@ $(document).ready(function(){
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function(){
 	if(xhr.readyState === 4){		
-			var employees = JSON.parse(xhr.responseText);	
-			console.log(typeof employees);
+			var employees = JSON.parse(xhr.responseText);
+			console.log(employees);	
+			var statusHTML = '';
+			for (var i = 0; i < employees.length; i ++){
+				if(employees[i].inoffice === true){
+					statusHTML += '<div class="col-xs-12 employee-btn"><a class="btn btn-success btn-xs in" role="button">IN</a>';
+				} else{
+					statusHTML += '<div class="col-xs-12 employee-btn"><a class="btn btn-danger btn-xs out" role="button">OUT</a>';
+				}
+				statusHTML += employees[i].name;
+				statusHTML += '</div>';
+			}
+			console.log(statusHTML);
+			document.getElementById('employees-list').innerHTML = statusHTML;
 	}
 };
 xhr.open('GET', 'data/employees.json');
